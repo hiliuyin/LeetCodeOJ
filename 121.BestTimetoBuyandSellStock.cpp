ad01289a6babdf77f02f9cc2683d5abce50d078c
YIN LIU <hiliuyin@163.com>
2016-05-31 14:56:02 +0800

    Create 121.BestTimetoBuyandSellStock.cpp

---
+++ 121.BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,25 @@
+// Say you have an array for which the ith element is the price of a given stock on day i.
+// If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
+// design an algorithm to find the maximum profit.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<int>::size_type sz = prices.size();
+        if (sz <= 1) return 0;
+
+        int minPriceEndingHere = prices[0], maxProfitSofar = 0;
+        for (vector<int>::size_type i = 1; i < sz; ++i)
+        {
+            if (prices[i] > minPriceEndingHere)
+            {
+                maxProfitSofar = std::max(maxProfitSofar, prices[i]-minPriceEndingHere);
+            }
+            else
+            {
+                minPriceEndingHere = prices[i];
+            }
+        }
+        return maxProfitSofar;
+    }
+};
