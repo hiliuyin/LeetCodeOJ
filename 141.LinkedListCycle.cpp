03e09004ba430a89ab63df258d19eee644e4df3d
YIN LIU <hiliuyin@163.com>
2016-06-01 15:43:42 +0800

    Update 141.LinkedListCycle.cpp

--- 141.LinkedListCycle.cpp
+++ 141.LinkedListCycle.cpp
@@ -6,17 +6,20 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == nullptr) return false;
-        if (head->next == nullptr) return false;
+        if (head == nullptr || head->next == nullptr) return false;
         
         for (auto fast = head->next->next, slow = head; fast != nullptr;)
         {
             if (slow == fast) return true;
             if (fast->next != nullptr)
+            {
                 fast = fast->next->next;
+                slow = slow->next;
+            }
             else
+            {
                 break;
-            slow = slow->next;
+            }
         }
         
         return false;
