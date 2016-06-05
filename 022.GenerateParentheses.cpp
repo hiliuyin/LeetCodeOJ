2e97e8fc77537db7eb74f11f48be98f98dd6de55
YIN LIU <hiliuyin@163.com>
2016-06-05 21:37:41 +0800

    Update 022.GenerateParentheses.cpp

--- 022.GenerateParentheses.cpp
+++ 022.GenerateParentheses.cpp
@@ -13,30 +13,34 @@ For example, given n = 3, a solution set is:
 
 class Solution {
 public:
-void generateHelper(std::vector<std::string> &result, std::string parth, int left, int right)
-{
-    if (left == 0 && right == 0)
-    {
-        result.emplace_back(parth);
-        return;
-    }
-    
-    if (left != 0)
-        generateHelper(result, parth+"(", left-1, right);
-    
-    if (left < right)
-        generateHelper(result, parth+")", left, right-1);
-}
 
 std::vector<std::string> generateParenthesis(int n)
 {
     if (n <= 0) return {};
 
     std::vector<std::string> result;
+    std::function<void(std::vector<std::string>&, std::string, int, int)> helper =
+    [&](std::vector<std::string> &result, std::string parth, int left, int right)
+    {
+        if (left == 0 && right == 0)
+        {
+            result.emplace_back(parth);
+            return;
+        }
+        
+        if (left != 0)
+            helper(result, parth+"(", left-1, right);
+        
+        if (left < right)
+            helper(result, parth+")", left, right-1);
+
+    };
+    
     std::string parth;
     
-    generateHelper(result, parth, n, n);
+    helper(result, parth, n, n);
     
     return result;
 }
+
 };
