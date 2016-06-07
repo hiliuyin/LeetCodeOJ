dbe12921e4b34fdad7876ded994158b5e3b178f2
YIN LIU <hiliuyin@163.com>
2016-06-07 15:24:06 +0800

    Create 118.PascalsTriangle.cpp

---
+++ 118.PascalsTriangle.cpp
@@ -0,0 +1,32 @@
+/*
+Given numRows, generate the first numRows of Pascal's triangle.
+For example, given numRows = 5,
+Return
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (numRows <= 0) return {};
+        
+        vector<int> row{1};
+        vector<vector<int>> result{row};
+        for (int i = 1; i < numRows; ++i)
+        {
+            result.emplace_back(row);
+            for (int j = 1; j < i; ++j)
+            {
+                result[i].emplace_back(result[i-1][j-1] + result[i-1][j]);
+            }
+            result[i].emplace_back(1);
+        }
+        return result;
+    }
+};
