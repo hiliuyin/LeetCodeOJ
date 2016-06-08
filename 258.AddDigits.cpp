282e2fbac811d9975d82b2935180ddd1329426ca
YIN LIU <hiliuyin@163.com>
2016-06-08 15:30:46 +0800

    Create 258.AddDigits.cpp

---
+++ 258.AddDigits.cpp
@@ -0,0 +1,19 @@
+/*
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+refer to:
+https://en.wikipedia.org/wiki/Digital_root
+*/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num < 10) return num;
+        return 1+(num-1)%9;
+    }
+};
