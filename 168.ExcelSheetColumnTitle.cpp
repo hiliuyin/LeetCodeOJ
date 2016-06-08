cdfc13aefe8b9befae78d5e39b7567ca9699ee55
YIN LIU <hiliuyin@163.com>
2016-06-08 21:39:58 +0800

    Create 168.ExcelSheetColumnTitle.cpp

---
+++ 168.ExcelSheetColumnTitle.cpp
@@ -0,0 +1,25 @@
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+*/
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        std::string result;
+        while (n > 0)
+        {
+            result.insert(0, 1, (n-1)%26 + 'A');
+            n = (n-1)/26;
+        }
+        return result;
+    }
+};
