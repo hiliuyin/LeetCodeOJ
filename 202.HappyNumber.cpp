15bcfdb821c6ff5854cd2be8af20eed4f292ba85
YIN LIU <hiliuyin@163.com>
2016-06-11 21:19:47 +0800

    Create 202.HappyNumber.cpp

---
+++ 202.HappyNumber.cpp
@@ -0,0 +1,39 @@
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+
+bool isHappy(int n)
+{
+    auto calc = [](int n) -> int
+    {
+        int result = 0;
+        while (n != 0)
+        {
+            result += std::pow(n%10, 2);
+            n = n/10;
+        }
+        return result;
+    };
+    
+    std::unordered_set<int> s{n};
+    int val = n;
+    for (;;)
+    {
+        val = calc(val);
+        if (val == 1)
+            return true;
+        
+        auto ret = s.emplace(val);
+        if (!ret.second)
+            break;
+    }
+    return false;
+}
