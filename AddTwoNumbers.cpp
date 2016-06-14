114e93d452a9779409e49f4de3e2da3837e5e5b5
YIN LIU <hiliuyin@163.com>
2016-06-14 14:29:48 +0800

    Update AddTwoNumbers.cpp

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -1,50 +1,44 @@
-// Add Two Numbers 
-// You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-// 
-// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-// Output: 7 -> 0 -> 8
-//
+/*
+You are given two linked lists representing two non-negative numbers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
 
-ListNode* doAddTwoNumbers(ListNode *l1, ListNode *l2, int carry)
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+*/
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
 {
-    int sum;
-    if (l1 == nullptr && l2 == nullptr)
+    std::function<void(ListNode*, ListNode*, int, ListNode*)> addHelper = [&](ListNode* l1, ListNode* l2, int carry, ListNode* prev)
     {
-        if (carry == 0)
-            return nullptr;
+        if (l1 == nullptr && l2 == nullptr)
+        {
+            prev->next = ((carry == 0) ? nullptr : new ListNode(carry));
+        }
+        else if (l1 == nullptr)
+        {
+            int sum = l2->val + carry;
+            carry = sum / 10;
+            prev->next = new ListNode(sum % 10);
+            addHelper(nullptr, l2->next, carry, prev->next);
+        }
+        else if (l2 == nullptr)
+        {
+            int sum = l1->val + carry;
+            carry = sum / 10;
+            prev->next = new ListNode(sum % 10);
+            addHelper(l1->next, nullptr, carry, prev->next);
+        }
         else
-            sum = carry;
-    }
-    else if (l1 == nullptr && l2 != nullptr)
-    {
-        sum = l2->val + carry;
-    }
-    else if (l1 != nullptr && l2 == nullptr)
-    {
-        sum = l1->val + carry;
-    }
-    else
-    {
-        sum = l1->val + l2->val + carry;
-    }
-    carry = sum/10;
-    int rem = sum%10;
-    
-    ListNode *p = new ListNode(rem);
-    
-    if (l1 == nullptr && l2 == nullptr)
-        p->next = doAddTwoNumbers(l1, l2, carry);
-    else if (l1 == nullptr && l2 != nullptr)
-        p->next = doAddTwoNumbers(nullptr, l2->next, carry);
-    else if (l1 != nullptr && l2 == nullptr)
-        p->next = doAddTwoNumbers(l1->next, nullptr, carry);
-    else
-        p->next = doAddTwoNumbers(l1->next, l2->next, carry);
+        {
+            int sum = l1->val + l2->val + carry;
+            carry = sum / 10;
+            prev->next = new ListNode(sum % 10);
+            addHelper(l1->next, l2->next, carry, prev->next);
+        }
+    };
 
-    return p;
-}
-
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-{
-    return doAddTwoNumbers(l1, l2, 0);
+    ListNode dummy(0);
+    addHelper(l1, l2, 0, &dummy);
+    return dummy.next;
 }
