1b0309ff7a74427d68be175aff1a1e75bcb9c492
YIN LIU <hiliuyin@163.com>
2016-06-16 17:52:01 +0800

    Update and rename GasStation.cpp to 134.GasStation.cpp

--- 134.GasStation.cpp
+++ 134.GasStation.cpp
@@ -1,12 +1,15 @@
-// Gas Station 
-// There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
-// 
-// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
-// 
-// Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
-// 
-// Note:
-// The solution is guaranteed to be unique.
+/*
+There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1).
+You begin the journey with an empty tank at one of the gas stations.
+Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+Note:
+The solution is guaranteed to be unique.
+*/
+
+
+
 
 int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
 {
@@ -45,14 +48,13 @@ int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
             }
             
             ++k;
+            k = (k >= stationNum) ? (k%stationNum) : k;
             
             if (k == startingStation)
             {
                 founded = true;
                 break;
             }
-            
-            k = (k >= stationNum) ? (k%stationNum) : k;
         } while (true);
         
         if (founded)
