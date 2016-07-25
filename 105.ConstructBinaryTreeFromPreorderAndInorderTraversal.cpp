29f270815d28f8ea9c345dc7b03069ef3ebd43e8
YIN LIU <hiliuyin@163.com>
2016-07-25 14:14:05 +0800

    Create 105.ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp

---
+++ 105.ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -0,0 +1,35 @@
+/*
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+*/
+
+TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder)
+{
+    if (preorder.size() != inorder.size()) return nullptr;
+    
+    std::function<TreeNode*(int, int, int, int)> helper = [&](int from1, int to1, int from2, int to2) -> TreeNode*
+    {
+        if (from1 > to1 || from2 > to2) return nullptr;
+        
+        int val = preorder[from1];
+        auto node = new TreeNode(val);
+        
+        int i = from2;
+        for (; i <= to2; ++i)
+        {
+            if (inorder[i] == val)
+                break;
+        }
+
+        node->left = helper(from1+1, from1 + i - from2, from2, i - 1);
+        node->right = helper(from1 + i - from2 + 1, to1, i+1, to2);
+        
+        return node;
+    };
+    
+    auto root = helper(0, (int)preorder.size()-1, 0, (int)inorder.size()-1);
+    
+    return root;
+}
