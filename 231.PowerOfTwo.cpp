696571d461c0be627765c8553d2478373b83433f
YIN LIU <hiliuyin@163.com>
2016-07-29 11:21:22 +0800

    Update 231.PowerOfTwo.cpp

--- 231.PowerOfTwo.cpp
+++ 231.PowerOfTwo.cpp
@@ -2,6 +2,7 @@
 Given an integer, write a function to determine if it is a power of two.
 */
 
+// solution 1
 bool isPowerOfTwo(int n)
 {
     if (n == 0) return false;
@@ -14,3 +15,12 @@ bool isPowerOfTwo(int n)
     }
     return true;
 }
+
+// solution 2
+bool isPowerOfTwo(int n)
+{
+    if (n <= 0 || (n&(n-1)) != 0) return false;
+    
+    return true;
+}
+
