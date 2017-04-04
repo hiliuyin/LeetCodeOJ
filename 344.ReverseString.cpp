f83ea7d8c08d3cac757e0aca05e0e855531122f5
YIN LIU <hiliuyin@163.com>
2017-04-04 14:10:44 +0800

    Update 344.ReverseString.cpp

--- 344.ReverseString.cpp
+++ 344.ReverseString.cpp
@@ -8,14 +8,9 @@ Given s = "hello", return "olleh".
 class Solution {
 public:
     string reverseString(string s) {
-        if (s.empty()) return s;
-        
-        const int sz = s.size();
-        for (int low = 0, high = sz-1; low <= high; ++low, --high)
-        {
-            auto temp = s[low];
-            s[low] = s[high];
-            s[high] = temp;
+        int low = 0, high = static_cast<int>(s.size()) - 1;
+        while (low < high) {
+            std::swap(s[low++], s[high--]);
         }
         return s;
     }
