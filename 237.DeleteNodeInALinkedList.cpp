ce2b8363be88b71787f06e44afdeff7609601975
YIN LIU <hiliuyin@163.com>
2017-04-04 21:53:15 +0800

    Update 237.DeleteNodeInALinkedList.cpp

--- 237.DeleteNodeInALinkedList.cpp
+++ 237.DeleteNodeInALinkedList.cpp
@@ -3,10 +3,7 @@ Write a function to delete a node (except the tail) in a singly linked list, giv
 Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
 */
 
-class Solution {
-public:
-    void deleteNode(ListNode* node) {
-        node->val = node->next->val;
-        node->next = node->next->next;
-    }
-};
+void deleteNode(ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
