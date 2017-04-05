08e9e37b889b4777662f6ab1e9b365268c7373b4
YIN LIU <hiliuyin@163.com>
2017-04-05 19:00:54 +0800

    Update 002.AddTwoNumbers.cpp

--- 002.AddTwoNumbers.cpp
+++ 002.AddTwoNumbers.cpp
@@ -7,36 +7,28 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 */
 
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-{
-    std::function<void(ListNode*, ListNode*, int, ListNode*)> addHelper = [&](ListNode* l1, ListNode* l2, int carry, ListNode* prev)
-    {
-        if (l1 == nullptr && l2 == nullptr)
-        {
-            prev->next = ((carry == 0) ? nullptr : new ListNode(carry));
-        }
-        else if (l1 == nullptr)
-        {
-            int sum = l2->val + carry;
-            carry = sum / 10;
-            prev->next = new ListNode(sum % 10);
-            addHelper(nullptr, l2->next, carry, prev->next);
-        }
-        else if (l2 == nullptr)
-        {
-            int sum = l1->val + carry;
-            carry = sum / 10;
-            prev->next = new ListNode(sum % 10);
-            addHelper(l1->next, nullptr, carry, prev->next);
-        }
-        else
-        {
-            int sum = l1->val + l2->val + carry;
-            carry = sum / 10;
-            prev->next = new ListNode(sum % 10);
-            addHelper(l1->next, l2->next, carry, prev->next);
-        }
-    };
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    std::function<void(ListNode*, ListNode*, int, ListNode*)> addHelper =
+        [&](ListNode* node1, ListNode* node2, int carry, ListNode* prev) {
+            if (node1 == nullptr && node2 == nullptr) {
+                prev->next = carry == 0 ? nullptr : new ListNode(carry);
+            }
+            else if (node1 == nullptr) {
+                int sum = node2->val + carry;
+                prev->next = new ListNode(sum % 10);
+                addHelper(nullptr, node2->next, sum/10, prev->next);
+            }
+            else if (node2 == nullptr) {
+                int sum = node1->val + carry;
+                prev->next = new ListNode(sum % 10);
+                addHelper(node1->next, nullptr, sum/10, prev->next);
+            }
+            else {
+                int sum = node1->val + node2->val + carry;
+                prev->next = new ListNode(sum % 10);
+                addHelper(node1->next, node2->next, sum/10, prev->next);
+            }
+        };
 
     ListNode dummy(0);
     addHelper(l1, l2, 0, &dummy);
