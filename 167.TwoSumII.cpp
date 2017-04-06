7e8b32b37388a39b22b92e9f96dcdbdbb82767b8
YIN LIU <hiliuyin@163.com>
2017-04-06 17:37:28 +0800

    Update 167.TwoSumII.cpp

--- 167.TwoSumII.cpp
+++ 167.TwoSumII.cpp
@@ -10,29 +10,32 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
 
-std::vector<int> twoSum(std::vector<int>& numbers, int target)
-{
-    auto binarySearch = [&numbers](int low, int high, int target) -> int
-    {
-        while (low <= high)
-        {
+// Solution 1: binary search, O(nlogn)
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    auto binarySearch = [&nums](int low, int high, int target) -> int {
+        while (low <= high) {
             int mid = low + (high-low)/2;
-            if (numbers[mid] == target) return mid;
-            else if (numbers[mid] > target)
-                high = mid-1;
-            else
-                low = mid+1;
-        }
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        };
         return -1;
     };
-    
-    for (int i = 0, iEnd = (int)numbers.size(); i < iEnd; ++i)
-    {
-        auto index = binarySearch(i+1, iEnd-1, target-numbers[i]);
-        if (index != -1)
-        {
+
+    for (int i = 0, iEnd = nums.size()-1; i < iEnd; ++i) {
+        int index = binarySearch(i+1, iEnd, target-nums[i]);
+        if (index > i)
             return {i+1, index+1};
-        }
     }
     return {-1, -1};
+}
+
+// Solution2: O(n)
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+    while (nums[low] + nums[high] != target) {
+        if (nums[low] + nums[high] > target) high--;
+        else low++;
+    }
+    return {low+1, high+1};
 }
