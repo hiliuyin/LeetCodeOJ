93f302b0755d21ce47eb5a348097f5c7da128813
YIN LIU <hiliuyin@163.com>
2017-04-06 20:34:51 +0800

    Update 001.TwoSum.cpp

--- 001.TwoSum.cpp
+++ 001.TwoSum.cpp
@@ -11,18 +11,16 @@ UPDATE (2016/2/13):
 The return format had been changed to zero-based indices. Please read the above updated description carefully.
 */
 
-std::vector<int> twoSum(std::vector<int>& nums, int target)
-{
+// Solution 1: O(nlogn) time complexity and O(n) extra space.
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
     if (nums.empty()) return{};
 
     std::vector<int> numsCopy(nums);
     std::sort(numsCopy.begin(), numsCopy.end());
 
-    auto bSearch = [&numsCopy](int begin, int last, int val) -> int
-    {
+    auto bSearch = [&numsCopy](int begin, int last, int val) -> int {
         if (begin > last) return -1;
-        while (begin <= last)
-        {
+        while (begin <= last) {
             int mid = begin + (last - begin) / 2;
             if (numsCopy[mid] == val) return mid;
             else if (numsCopy[mid] > val) last = mid - 1;
@@ -31,11 +29,9 @@ std::vector<int> twoSum(std::vector<int>& nums, int target)
         return -1;
     };
 
-    for (int i = 0, iEnd = numsCopy.size(); i < iEnd; ++i)
-    {
+    for (int i = 0, iEnd = numsCopy.size(); i < iEnd; ++i) {
         int index = bSearch(i+1, iEnd-1, target- numsCopy[i]);
-        if (index != -1)
-        { 
+        if (index != -1) { 
             auto it1 = std::find(nums.begin(), nums.end(), numsCopy[i]);
             auto it2 = std::find(nums.begin(), nums.end(), numsCopy[index]);
             if (it2 == it1)
@@ -44,4 +40,15 @@ std::vector<int> twoSum(std::vector<int>& nums, int target)
         }
     }
     return{};
+}
+
+// Solution 2: O(n)
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> M;
+    for (int i = 0, iEnd = nums.size(); i < iEnd; ++i) {
+        if (M.find(target-nums[i]) != M.end()) {
+            return {M[target-nums[i]], i};
+        }
+        M[nums[i]] = i;
+    }
 }
