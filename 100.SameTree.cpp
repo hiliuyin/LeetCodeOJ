e5c01c9623d078090e76249620f4a602d6c6fd85
YIN LIU <hiliuyin@163.com>
2017-04-10 18:25:00 +0800

    Update 100.SameTree.cpp

--- 100.SameTree.cpp
+++ 100.SameTree.cpp
@@ -13,12 +13,11 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
-        if (p == NULL && q != NULL) return false;
-        if (p != NULL && q == NULL) return false;
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr && q != nullptr) return false;
+        if (p != nullptr && q == nullptr) return false;
         if (p->val != q->val) return false;
-        if (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right)) return true;
-        
+        if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right)) return true;
         return false;
     }
 };
