369394cf39e00d7963a531f64bcb160610e86a71
YIN LIU <hiliuyin@163.com>
2017-04-11 17:49:19 +0800

    Update 217.ContainsDuplicate.cpp

--- 217.ContainsDuplicate.cpp
+++ 217.ContainsDuplicate.cpp
@@ -9,11 +9,15 @@ bool containsDuplicate(std::vector<int>& nums)
     if (nums.empty()) return false;
 
     std::unordered_set<int> s;
-    for (auto it = nums.cbegin(); it != nums.cend(); ++it)
-    {
+    for (auto it = nums.cbegin(); it != nums.cend(); ++it) {
         auto ret = s.emplace(*it);
         if (!ret.second)
             return true;
     }
     return false;
+}
+
+// An elegant code in C++
+bool containsDuplicate(std::vector<int>& nums) {
+    return nums.size() > std::unordered_set<int>(nums.begin(), nums.end()).size();
 }
