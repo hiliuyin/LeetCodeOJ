9dd2a862caef64a7fc342573365af66d76e4c81d
YIN LIU <hiliuyin@163.com>
2017-04-11 23:30:47 +0800

    Update 347.TopKFrequentElements.cpp

--- 347.TopKFrequentElements.cpp
+++ 347.TopKFrequentElements.cpp
@@ -9,36 +9,47 @@ You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 */
 
-std::vector<int> topKFrequent(std::vector<int>& nums, int k)
-{
-    std::unordered_map<int, int> U;
-    for (int i = 0, iEnd = (int)nums.size(); i < iEnd; ++i)
-    {
-        if (U.find(nums[i]) != U.end())
-            U[nums[i]]++;
-        else
-            U.emplace(std::make_pair(nums[i], 1));
+// Solution 1: using std::priority_queue
+std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> m;
+    for (const auto& num : nums) {
+        m[num]++;
     }
     
-    std::vector<std::pair<int, int>> V;
-    for (auto it = U.cbegin(); it != U.cend(); ++it)
-    {
-        V.emplace_back(*it);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+    for (auto it = m.begin(); it != m.end(); ++it) {
+        pq.emplace(it->second, it->first);
+        if (pq.size() > k) pq.pop();
     }
-    
-    std::make_heap(V.begin(), V.end(),
-                   [](const std::pair<int, int> &a, const std::pair<int, int> &b)
-                   { return a.second < b.second; });
     
     std::vector<int> result;
-    while (k-- > 0)
-    {
-        std::pop_heap(V.begin(), V.end(),
-                      [](const std::pair<int, int> &a, const std::pair<int, int> &b)
-                      { return a.second < b.second; });
-        result.emplace_back(V.back().first);
-        V.pop_back();
+    while (!pq.empty()) {
+        result.emplace_back(pq.top().second);
+        pq.pop();
+    }
+    return result;
+}
+
+// Solution 2: using std::make_heap
+std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> m;
+    for (const auto& num : nums) {
+        m[num]++;
     }
     
+    std::vector<std::pair<int, int>> vec;
+    for (auto it = m.begin(); it != m.end(); ++it) {
+        vec.emplace_back(it->second, it->first);
+    }
+    
+    std::make_heap(vec.begin(), vec.end(),
+        [](const std::pair<int, int>& a, const std::pair<int, int>& b) { return a.first < b.first; });
+    
+    std::vector<int> result;
+    while (k-- > 0) {
+        std::pop_heap(vec.begin(), vec.end());
+        result.emplace_back(vec.back().second);
+        vec.pop_back();
+    }
     return result;
 }
