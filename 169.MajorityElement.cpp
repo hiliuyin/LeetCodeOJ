3755da8948e75667dc22c65416822bc0c8045911
YIN LIU <hiliuyin@163.com>
2017-04-17 20:09:21 +0800

    Update 169.MajorityElement.cpp

--- 169.MajorityElement.cpp
+++ 169.MajorityElement.cpp
@@ -2,8 +2,31 @@
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 You may assume that the array is non-empty and the majority element always exist in the array.
 */
-int majorityElement(vector<int>& nums)
-{
+
+// Solution 1: sort
+int majorityElement(vector<int>& nums) {
     std::sort(nums.begin(), nums.end());
     return nums[nums.size()/2];
+}
+
+// Solution 2: partial sort
+int majorityElement(vector<int>& nums) {
+    std::nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
+    return nums[nums.size()/2];
+}
+
+// Solution 3: moore voting algorithm
+int majorityElement(vector<int>& nums) {
+    int elem;
+    int count = 0;
+    for (const auto& num : nums) {
+        if (count == 0) {
+            elem = num;
+            count++;
+        }
+        else {
+            num == elem ? count++ : count--;
+        }
+    }
+    return elem;
 }
