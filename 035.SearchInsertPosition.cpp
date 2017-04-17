47ff7f2a89c7b14f863d9910bbc507092f3aa928
YIN LIU <hiliuyin@163.com>
2017-04-17 23:52:38 +0800

    Update 035.SearchInsertPosition.cpp

--- 035.SearchInsertPosition.cpp
+++ 035.SearchInsertPosition.cpp
@@ -13,22 +13,15 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         if (nums.empty()) return -1;
         
-        auto binarySearch = [&nums, target]() -> int 
-        {
+        auto binarySearch = [&nums, target]() -> int {
             int low = 0, high = nums.size()-1;
-            while (low <= high)
-            {
+            
+            // Invariant: the desired index is between [low, high+1]
+            while (low <= high) {
                 int mid = low + (high-low)/2;
-                if (nums[mid] == target)
-                    return mid;
-                else if (nums[mid] > target)
-                {
-                    high = mid-1;
-                }
-                else // nums[mid] < target
-                {
-                    low = mid+1;
-                }
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] > target) high = mid-1;
+                else low = mid+1;
             }
             return low;
         };
