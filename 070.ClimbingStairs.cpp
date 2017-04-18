07b12a919a62deadc83d21d459cd2525f9cbbfcf
YIN LIU <hiliuyin@163.com>
2017-04-18 12:13:59 +0800

    Update 070.ClimbingStairs.cpp

--- 070.ClimbingStairs.cpp
+++ 070.ClimbingStairs.cpp
@@ -1,16 +1,13 @@
 // You are climbing a stair case. It takes n steps to reach to the top.
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
+// Dynamic Programming: Fibonacci
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n < 1) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2; // 1,1 or 2
-        
+        if (n == 0 || n == 1 || n == 2) return n;
         int waysN, waysN_1 = 2, waysN_2 = 1;
-        for (int i = 3; i <= n; ++i)
-        {
+        for (int i = 3; i <= n; ++i) {
             waysN = waysN_1 + waysN_2;
             waysN_2 = waysN_1;
             waysN_1 = waysN;
