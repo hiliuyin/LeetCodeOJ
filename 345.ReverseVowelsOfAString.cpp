f7742ab118f8506aea1523d5b4f89291a89ab9e4
YIN LIU <hiliuyin@163.com>
2017-04-18 21:51:05 +0800

    Update 345.ReverseVowelsOfAString.cpp

--- 345.ReverseVowelsOfAString.cpp
+++ 345.ReverseVowelsOfAString.cpp
@@ -12,10 +12,8 @@ std::string reverseVowels(std::string s)
 {
     if (s.size() <= 1) return s;
     
-    auto isVowel = [](char ch) -> bool
-    {
-        switch(ch)
-        {
+    auto isVowel = [](char ch) -> bool {
+        switch(ch) {
         case 'a':
         case 'A':
         case 'e':
@@ -32,22 +30,30 @@ std::string reverseVowels(std::string s)
         }
     };
     
-    for (int i = 0, j = (int)s.size()-1; i < j; ++i, --j)
-    {
-        for (int iEnd = (int)s.size()-1; i < iEnd; ++i)
-        {
+    for (int i = 0, j = (int)s.size()-1; i < j; ++i, --j) {
+        for (int iEnd = (int)s.size()-1; i < iEnd; ++i) {
             if (isVowel(s[i]))
                 break;
         }
         
-        for (int jEnd = 0; j >= jEnd; --j)
-        {
+        for (int jEnd = 0; j >= jEnd; --j) {
             if (isVowel(s[j]))
                 break;
         }
         
         if (i < j)
             std::swap(s[i], s[j]);
+    }
+    return s;
+}
+
+// solution 2: use std::string API
+std::string reverseVowels(std::string s) {
+    std::string::size_type low = s.find_first_of("aeiou", 0), high = s.find_last_of("aeiou", std::string::npos);
+    while (low < high) {
+        std::swap(s[low], s[high]);
+        low = s.find_first_of("aeiou", low+1);
+        high = s.find_last_of("aeiou", high-1);
     }
     return s;
 }
