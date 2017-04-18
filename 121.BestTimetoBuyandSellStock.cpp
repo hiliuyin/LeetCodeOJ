5d44462d5d400f029601492db0ba152d70242161
YIN LIU <hiliuyin@163.com>
2017-04-18 19:01:29 +0800

    Update 121.BestTimetoBuyandSellStock.cpp

--- 121.BestTimetoBuyandSellStock.cpp
+++ 121.BestTimetoBuyandSellStock.cpp
@@ -2,25 +2,15 @@
 // If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
 // design an algorithm to find the maximum profit.
 
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        vector<int>::size_type sz = prices.size();
-        if (sz <= 1) return 0;
-
-        int minPriceEndingHere = prices[0], maxProfitSofar = 0;
-        for (vector<int>::size_type i = 1; i < sz; ++i)
-        {
-            int price = prices[i];
-            if (price > minPriceEndingHere)
-            {
-                maxProfitSofar = std::max(maxProfitSofar, price-minPriceEndingHere);
-            }
-            else
-            {
-                minPriceEndingHere = price;
-            }
-        }
-        return maxProfitSofar;
+int maxProfit(vector<int>& prices) {
+    if (prices.size() <= 1) return 0;
+        
+    int maxProfit = 0, minPrice = prices[0];
+    for (int i = 1, iEnd = prices.size(); i < iEnd; ++i) {
+        if (prices[i] > minPrice)
+            maxProfit = std::max(maxProfit, prices[i] - minPrice);
+        else
+            minPrice = std::min(prices[i], minPrice);
     }
-};
+    return maxProfit;
+}
