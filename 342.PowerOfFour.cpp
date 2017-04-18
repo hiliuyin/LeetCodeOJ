5f4c7fe8d0e073c919c8281cb4dfd27f79d8557f
YIN LIU <hiliuyin@163.com>
2017-04-18 17:22:57 +0800

    Update 342.PowerOfFour.cpp

--- 342.PowerOfFour.cpp
+++ 342.PowerOfFour.cpp
@@ -7,14 +7,7 @@ Given num = 16, return true. Given num = 5, return false.
 Follow up: Could you solve it without loops/recursion?
 */
 
-bool isPowerOfFour(int num)
-{
-    if (num <= 0) return false;
-
-    if ((num & (num - 1)) != 0) return false;
-
-    const int mask = 0b01010101010101010101010101010101;
-    if ((num & mask) != 0) return true;
-
-    return false;
+bool isPowerOfFour(int num) {
+    if (num <= 0 || (num & (num-1)) != 0) return false;
+    return (num & 0x55555555) != 0;
 }
