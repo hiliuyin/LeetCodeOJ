a94ea148abfff8e33470be7ccb2af62394b45ce2
YIN LIU <hiliuyin@163.com>
2017-04-19 00:07:37 +0800

    Update 027.RemoveElement.cpp

--- 027.RemoveElement.cpp
+++ 027.RemoveElement.cpp
@@ -1,31 +1,26 @@
 /*
 Given an array and a value, remove all instances of that value in place and return the new length.
-
 Do not allocate extra space for another array, you must do this in place with constant memory.
-
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
 Example:
 Given input array nums = [3,2,2,3], val = 3
-
 Your function should return length = 2, with the first two elements of nums being 2.
 */
 
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        if (nums.empty()) return 0;
-        
-        auto insertPos = nums.begin();
-        for (auto it = nums.begin(); it != nums.end(); ++it)
-        {
-            if (*it != val)
-            {
-                *insertPos = *it;
-                insertPos++;
-            }
+int removeElement(vector<int>& nums, int val) {
+    if (nums.empty()) return 0;
+    
+    int i = 0, j = nums.size()-1;
+    while (i <= j) {
+        if (nums[i] == val) {
+            while (j >= 0 && nums[j] == val) j--;
+            if (i > j) break;
+            std::swap(nums[i++], nums[j--]);
         }
-        nums.erase(insertPos, nums.end());
-        return nums.size();
+        else {
+            i++;
+        }
     }
-};
+    return j+1;
+}
