213109e050abb4d83c7d8afc53cdad282114fd94
YIN LIU <hiliuyin@163.com>
2017-04-18 18:25:49 +0800

    Update 053.MaximumSubarray.cpp

--- 053.MaximumSubarray.cpp
+++ 053.MaximumSubarray.cpp
@@ -2,15 +2,11 @@
 // For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
 // the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int maxendinghere = nums[0], maxsofar = nums[0]; // because nums containing at least one number
-        for (vector<int>::const_iterator it = nums.cbegin()+1; it != nums.cend(); ++it)
-        {
-            maxendinghere = std::max(maxendinghere+*it, *it);
-            maxsofar = std::max(maxsofar, maxendinghere);
-        }
-        return maxsofar;
+int maxSubArray(vector<int>& nums) {
+    int maxendinghere = nums[0], maxsofar = nums[0];
+    for (int i = 1, iEnd = nums.size(); i < iEnd; ++i) {
+        maxendinghere = std::max(maxendinghere + nums[i], nums[i]);
+        maxsofar = std::max(maxsofar, maxendinghere);
     }
-};
+    return maxsofar;
+}
