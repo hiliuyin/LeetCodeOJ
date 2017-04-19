1b6350ada7503d8e957f55524cf9ff3090693e83
YIN LIU <hiliuyin@163.com>
2017-04-19 22:38:36 +0800

    Update 066.PlusOne.cpp

--- 066.PlusOne.cpp
+++ 066.PlusOne.cpp
@@ -1,17 +1,30 @@
 // Given a non-negative number represented as an array of digits, plus one to the number.
 // The digits are stored such that the most significant digit is at the head of the list.
 
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
-        int carry = 1;
-        for (vector<int>::reverse_iterator ri = digits.rbegin(); ri != digits.rend(); ++ri)
-        {
-            int digit = *ri + carry;
-            *ri = digit%10;
-            carry = digit/10;
-        }
-        if (carry != 0) digits.insert(digits.begin(), carry);
-        return digits;
+// Solution 1:
+vector<int> plusOne(vector<int>& digits) {
+    int carry = 1;
+    for (auto ri = digits.rbegin(); ri != digits.rend(); ++ri) {
+        int digit = *ri + carry;
+        *ri = digit%10;
+        carry = digit/10;
     }
-};
+    if (carry != 0) digits.insert(digits.begin(), carry);
+    return digits;
+}
+
+// Solution 2: Very simple
+vector<int> plusOne(vector<int>& digits) {
+    for (int i = digits.size()-1; i >= 0; --i) {
+        if (digits[i] == 9) {
+            digits[i] = 0;
+        }
+        else {
+            digits[i]++;
+            return digits;
+        }
+    }
+    digits[0] = 1;
+    digits.emplace_back(0);
+    return digits;
+}
