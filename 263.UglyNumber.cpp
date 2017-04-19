aff8da569c60907154b7aa510e46cfa44226c464
YIN LIU <hiliuyin@163.com>
2017-04-19 21:21:50 +0800

    Update 263.UglyNumber.cpp

--- 263.UglyNumber.cpp
+++ 263.UglyNumber.cpp
@@ -6,19 +6,12 @@ For example, 6, 8 are ugly while 14 is not ugly since it includes another prime 
 
 Note that 1 is typically treated as an ugly number.
 */
-bool isUgly(int num)
-{
+
+bool isUgly(int num) {
     if (num <= 0) return false;
-        
     if (num == 1) return true;
         
-    while (num%5 == 0)
-        num /= 5;
-            
-    while (num%3 == 0)
-        num /= 3;
-
-    if ((num & (num-1)) == 0) return true;
-        
-    return false;
+    while (num%5 == 0) num /= 5; 
+    while (num%3 == 0) num /= 3;
+    return ((num & (num-1)) == 0);
 }
