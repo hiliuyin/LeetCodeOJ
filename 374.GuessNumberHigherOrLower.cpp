126372b920c730c90ece391d338c5beb8a8e637e
YIN LIU <hiliuyin@163.com>
2017-04-20 17:56:34 +0800

    Update 374.GuessNumberHigherOrLower.cpp

--- 374.GuessNumberHigherOrLower.cpp
+++ 374.GuessNumberHigherOrLower.cpp
@@ -18,20 +18,13 @@ Return 6.
 // @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
 int guess(int num);
 
-class Solution {
-public:
-    int guessNumber(int n) {
-        int low = 1, high = n;
-        while (low <= high)
-        {
-            int mid = low + (high-low)/2;
-            if (guess(mid) == -1)
-                high = mid-1;
-            else if (guess(mid) == 1)
-                low = mid+1;
-            else
-                return mid;
-        }
-        return low;
+int guessNumber(int n) {
+    int low = 1, high = n, mid;
+    while (low <= high) {
+        mid = low + (high-low)/2;
+        if (guess(mid) == 0) break;
+        else if (guess(mid) == 1) low = mid+1;
+        else high = mid-1;
     }
-};
+    return mid;
+}
