01b230f229a83c1f96b70d243b4839d3bbcc157d
YIN LIU <hiliuyin@163.com>
2017-04-20 14:41:42 +0800

    Update 119.PascalsTriangleII.cpp

--- 119.PascalsTriangleII.cpp
+++ 119.PascalsTriangleII.cpp
@@ -7,11 +7,12 @@ Return [1,3,3,1].
 
 std::vector<int> getRow(int rowIndex) {
     if (rowIndex < 0) return {};
-    
-    std::vector<int> row(rowIndex+1, 1);
-    for (int i = 1; i < rowIndex; ++i)
-        for (int j = i; j >= 1; --j)
-            row[j] = row[j]+row[j-1];
-    
-    return row;
+
+    std::vector<int> result(rowIndex+1, 1);
+    for (int i = 2; i <= rowIndex; ++i) {
+        for (int j = i-1; j > 0; --j) {
+            result[j] = result[j] + result[j-1];
+        }
+    }
+    return result;
 }
