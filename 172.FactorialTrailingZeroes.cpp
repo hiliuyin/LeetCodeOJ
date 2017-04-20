5726f2010cf90e86f7b956ff309edcd3a11da814
YIN LIU <hiliuyin@163.com>
2017-04-20 21:56:01 +0800

    Update 172.FactorialTrailingZeroes.cpp

--- 172.FactorialTrailingZeroes.cpp
+++ 172.FactorialTrailingZeroes.cpp
@@ -5,12 +5,10 @@ Note: Your solution should be in logarithmic time complexity.
 */
 
 // 只需考虑5的个数，5, 25, 125....
-int trailingZeroes(int n) 
-{
+int trailingZeroes(int n)  {
     long long base = 5; // 防止溢出
     int zeros = 0;
-    while (n >= base)
-    {
+    while (n >= base) {
         zeros += n/base;
         base *= 5;
     }
