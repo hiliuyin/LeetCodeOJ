d675307b9b3b3d119f92a309ec61510c956ec49e
YIN LIU <hiliuyin@163.com>
2017-04-20 17:04:26 +0800

    Update 141.LinkedListCycle.cpp

--- 141.LinkedListCycle.cpp
+++ 141.LinkedListCycle.cpp
@@ -3,25 +3,13 @@
 // Follow up:
 // Can you solve it without using extra space?
 
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        if (head == nullptr || head->next == nullptr) return false;
-        
-        for (auto fast = head->next->next, slow = head; fast != nullptr;)
-        {
-            if (slow == fast) return true;
-            if (fast->next != nullptr)
-            {
-                fast = fast->next->next;
-                slow = slow->next;
-            }
-            else
-            {
-                break;
-            }
-        }
-        
-        return false;
+bool hasCycle(ListNode *head) {
+    ListNode* fast = head, *slow = head;
+    for (;;) {
+        if (fast == nullptr || fast->next == nullptr) break;
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
     }
-};
+    return false;
+}
