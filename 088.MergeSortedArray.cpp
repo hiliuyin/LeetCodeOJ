a94a3b7cc2ddd33d8b552e1d63665579c2e75f59
YIN LIU <hiliuyin@163.com>
2017-04-20 22:44:57 +0800

    Update 088.MergeSortedArray.cpp

--- 088.MergeSortedArray.cpp
+++ 088.MergeSortedArray.cpp
@@ -6,29 +6,20 @@ You may assume that nums1 has enough space (size that is greater or equal to m +
 The number of elements initialized in nums1 and nums2 are m and n respectively.
 */
 
-void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
-{
-    for (int i = m - 1, j = n - 1, k = m + n - 1; k >= 0; --k)
-    {
-        if (i >= 0 && j >= 0)
-        {
-            if (nums1[i] > nums2[j])
-            {
-                nums1[k] = nums1[i];
-                i--;
-            }
-            else
-            {
-                nums1[k] = nums2[j];
-                j--;
-            }
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m-1, j = n-1, k = 0;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[m+n-1-k] = nums1[i--];
         }
-        else if (j >= 0)
-        {
-            for (int jj = 0; jj <= j; ++jj)
-            {
-                nums1[jj] = nums2[jj];
-            }
+        else {
+            nums1[m+n-1-k] = nums2[j--];
         }
+        ++k;
+    }
+
+    while (j >= 0) {
+        nums1[j] = nums2[j];
+        --j;
     }
 }
