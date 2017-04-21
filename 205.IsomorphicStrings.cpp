45ee5d847d0ccd3cef45eb561d0727523b6c365d
YIN LIU <hiliuyin@163.com>
2017-04-21 11:05:03 +0800

    Update 205.IsomorphicStrings.cpp

--- 205.IsomorphicStrings.cpp
+++ 205.IsomorphicStrings.cpp
@@ -13,31 +13,17 @@ Note:
 You may assume both s and t have the same length.
 */
 
-bool isIsomorphic(std::string s, std::string t)
-{
+bool isIsomorphic(std::string s, std::string t) {
     std::map<char, char> m1, m2;
-    for (int i = 0, iEnd = (int)s.size(); i < iEnd; ++i)
-    {
-        if (m1.find(s[i]) != m1.end())
-        {
-            if(m1[s[i]] != t[i])
-                return false;
+    for (int i = 0, iEnd = s.size(); i < iEnd; ++i) {
+        if (m1.find(s[i]) != m1.end()) {
+            if (m1[s[i]] != t[i]) return false;
         }
-        else
-        {
-            m1.emplace(std::make_pair(s[i], t[i]));
+        if (m2.find(t[i]) != m2.end()) {
+            if (m2[t[i]] != s[i]) return false;
         }
-        
-        if (m2.find(t[i]) != m2.end())
-        {
-            if(m2[t[i]] != s[i])
-                return false;
-        }
-        else
-        {
-            m2.emplace(std::make_pair(t[i], s[i]));
-        }
+        m1[s[i]] = t[i];
+        m2[t[i]] = s[i];
     }
-    
     return true;
 }
