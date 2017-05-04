c71c870c016c44a472819a7d49d5cb77c9506691
YIN LIU <hiliuyin@163.com>
2017-05-04 12:33:20 +0800

    Update 110.BalancedBinaryTree.cpp

--- 110.BalancedBinaryTree.cpp
+++ 110.BalancedBinaryTree.cpp
@@ -5,19 +5,32 @@ For this problem, a height-balanced binary tree is defined as a binary tree
 in which the depth of the two subtrees of every node never differ by more than 1.
 */
 
-bool isBalanced(TreeNode* root)
-{
+/*
+From the problem description,
+a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+Below is a representation of the tree input: {1,2,2,3,3,3,3,4,4,4,4,4,4,#,#,5,5}:
+
+        ____1____
+       /         \
+      2           2
+     /  \        / \
+    3    3      3   3
+   /\    /\    /\
+  4  4  4  4  4  4 
+ /\
+5  5
+Let's start with the root node (1). As you can see, left subtree's depth is 5, while right subtree's depth is 4. Therefore, the condition for a height-balanced binary tree holds for the root node. We continue the same comparison recursively for both left and right subtree, and we conclude that this is indeed a balanced binary tree.
+*/
+
+bool isBalanced(TreeNode* root) {
     if (root == nullptr) return true;
-    
-    std::function<int(TreeNode*)> maxDepth = [&](TreeNode *root) -> int
-    {
-        if (root == nullptr)
-            return 0;
-        else
-            return std::max(maxDepth(root->left), maxDepth(root->right))+1;
+
+    std::function<int(TreeNode*)> getHeight = [&](TreeNode* node) {
+        if (node == nullptr) return 0;
+        return std::max(getHeight(node->left), getHeight(node->right)) + 1;
     };
-    
-    int delta = std::abs(maxDepth(root->left) - maxDepth(root->right));
-    
-    return isBalanced(root->left) && isBalanced(root->right) && delta <=1;
+
+    return std::abs(getHeight(root->left) - getHeight(root->right)) <= 1
+        && isBalanced(root->left)
+        && isBalanced(root->right);
 }
