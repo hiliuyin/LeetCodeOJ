31f3bb322c2579efec1b2f941c6435f5f52d70df
YIN LIU <hiliuyin@163.com>
2017-05-07 22:25:08 +0800

    Update 190.ReverseBits.cpp

--- 190.ReverseBits.cpp
+++ 190.ReverseBits.cpp
@@ -8,12 +8,21 @@ Follow up:
 If this function is called many times, how would you optimize it?
 */
 
-uint32_t reverseBits(uint32_t n)
-{
+// Solution 1:
+uint32_t reverseBits(uint32_t n) {
     const std::vector<int> reversedMap = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
-    
     return (reversedMap[n & 0xF] << 28)      | (reversedMap[(n>>4) & 0xF] << 24)
          | (reversedMap[(n>>8) & 0xF] << 20) | (reversedMap[(n>>12) & 0xF] << 16)
          | (reversedMap[(n>>16) & 0xF] << 12)| (reversedMap[(n>>20) & 0xF] << 8)
          | (reversedMap[(n>>24) & 0xF] << 4) | (reversedMap[(n>>28) & 0xF]);
+}
+
+// Solution 2: for 8 bit binary number abcdefgh, the process is as follow: abcdefgh -> efghabcd -> ghefcdab -> hgfedcba
+uint32_t reverseBits(uint32_t n) {
+    n = (n >> 16) | (n << 16);
+    n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+    n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+    n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+    n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+    return n;
 }
