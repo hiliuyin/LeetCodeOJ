202f12d787c2e2e9f21608afe34594c82a2270e7
YIN LIU <hiliuyin@163.com>
2017-05-07 21:55:58 +0800

    Update 108.ConvertSortedArrayToBinarySearchTree.cpp

--- 108.ConvertSortedArrayToBinarySearchTree.cpp
+++ 108.ConvertSortedArrayToBinarySearchTree.cpp
@@ -10,21 +10,15 @@
  * };
  */
 
-TreeNode* sortedArrayToBST(std::vector<int>& nums)
-{
-    if (nums.empty()) return nullptr;
-    
-    std::function<TreeNode*(int, int)> helper = [&](int low, int high) -> TreeNode*
-    {
+TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+    std::function<TreeNode*(int, int)> helper = [&](int low, int high) -> TreeNode* {
         if (low > high) return nullptr;
-        if (low == high) return new TreeNode(nums[low]);
-        
-        int mid = low + (high-low)/2;
-        TreeNode *root = new TreeNode(nums[mid]);
+        int mid = low + (high - low)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
         root->left = helper(low, mid-1);
         root->right = helper(mid+1, high);
         return root;
     };
     
-    return helper(0, (int)nums.size()-1);
+    return helper(0, nums.size()-1);
 }
