d445a7436454c6ea332f73ecfd15bc227f9fdda5
YIN LIU <hiliuyin@163.com>
2017-12-05 21:22:34 +0800

    Create 717.1-bitAnd2-bitCharacters.cpp

---
+++ Algorithms/717.1-bitAnd2-bitCharacters.cpp
@@ -0,0 +1,32 @@
+/*
+We have two special characters. The first character can be represented by one bit 0.
+The second character can be represented by two bits (10 or 11).
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or not.
+The given string will always end with a zero.
+
+Example 1:
+Input: 
+bits = [1, 0, 0]
+Output: True
+Explanation: 
+The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+
+Example 2:
+Input: 
+bits = [1, 1, 1, 0]
+Output: False
+Explanation: 
+The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+Note:
+1 <= len(bits) <= 1000.
+bits[i] is always 0 or 1.
+*/
+
+bool isOneBitCharacter(vector<int>& bits) {
+    for (int i = 0, iEnd = bits.size(); i < iEnd; ) {
+        if (bits[i] == 1) i += 2;
+        else if (bits[i] == 0 && i == iEnd-1) return true;
+        else ++i;
+    }
+    return false;
+}
